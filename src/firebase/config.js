0d764b1a97325e886b7044658fcae51de9a8af1d
Tam Nguyen <33707686+tamdrashtri@users.noreply.github.com>
2021-03-13 00:10:02 -0800

    update firebase app

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,15 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAZXimGj1r-CFzjsnY7EdQ_I69ni_1MRck",
-  authDomain: "live-chat-bd94b.firebaseapp.com",
-  projectId: "live-chat-bd94b",
-  storageBucket: "live-chat-bd94b.appspot.com",
-  messagingSenderId: "420525990596",
-  appId: "1:420525990596:web:4b272a9cf97e8c43aa9f91"
+  apiKey: "AIzaSyD0n5tTxB1oPnJghXJSu03kCsH36kCq3w0",
+  authDomain: "aikon-e1ba6.firebaseapp.com",
+  projectId: "aikon-e1ba6",
+  storageBucket: "aikon-e1ba6.appspot.com",
+  messagingSenderId: "283145900428",
+  appId: "1:283145900428:web:662ea099761b4e244a06a9",
+  measurementId: "G-50JY4810PF"
 };
 
 // init firebase
